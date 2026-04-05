1c33dfd4f079b03ff14e13ade6a0900ba830f5fa
Kavyan Nayak <kavyannayak104@gmail.com>
2026-04-05 12:03:00 +0530

    task 1

--- Loops/forin.js
+++ Loops/forin.js
@@ -1,5 +1,25 @@
-let obj = {name: "Kavyan", age: 22}
+// let obj = {name: "Kavyan", age: 22}
+
+// for(let i in obj){
+//     console.log(i, obj[i]);
+// }
+
+
+
+// =============== Task =================
+
+
+let obj = {
+    a: 10,
+    b: 20,
+    c: 30
+};
+
+
+let count = 0;
 
 for(let i in obj){
-    console.log(i, obj[i]);
+    count++;
 }
+
+console.log("Total properties:", count);
