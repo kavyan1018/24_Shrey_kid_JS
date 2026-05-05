b2177cf5522ddba4d48851d45adec039722db1b6
Kavyan Nayak <kavyannayak104@gmail.com>
2026-05-05 11:20:18 +0530

    array

---
+++ Array/arrayDemo1.js
@@ -0,0 +1,31 @@
+// var a = [10, 20, 30, 40, 50];
+
+// var a = ["Apple", "Banana", "Orange", "Mango", "Grapes"];
+
+var a = [10, 20, 30, 40, 50];
+// console.log(a[0]);
+// console.log(a[3]);
+// console.log(a[4]);
+
+
+// array methods 
+/*
+    push - add element at the end of the array  
+    pop - remove element from the end of the array
+    unshift - add element at the beginning of the array
+    shift - remove element from the beginning of the array
+ */
+
+var a = [1, 2, 3, 4, 5];
+
+// a.push(6);
+// console.log(a);
+
+// a.pop();
+// console.log(a);
+
+// a.unshift(0);
+// console.log(a);
+
+a.shift();
+console.log(a);
